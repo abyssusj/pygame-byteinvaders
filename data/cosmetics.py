45f302bb06f9255990b2b3381d8fc912fb8d9402
abyssusj <abyssus.j@gmail.com>
2014-08-05 01:18:09 +1000

    restructing code into seperate file libaries

---
+++ data/cosmetics.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Classes that define colours and stuff
+
+class PaintBrush:
+
+    def __init__(self):
+        return
+
+    def swatch(self):
+        colours = {'red': (255,0,0), 'green': (0,255,0), 'blue': (0,0,255),
+                'darkBlue': (0,0,128), 'white': (255,255,255), 'black': (0,0,0),
+                'pink': (255,51,102), 'yellow': (255,255,0), 'vdarkBlue': (0,6,31)}
+        return colours
